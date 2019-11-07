b4e60ff8a64209a45b716dc77950b1dcb8626083
Mikołaj Baran <tarralan@wp.pl>
2019-11-07 01:31:31 +0100

    Dodanie CrossOrigin oraz usuniecie testEndpoint

--- src/main/java/com/example/demo/endpoints/BoardController.java
+++ src/main/java/com/example/demo/endpoints/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/table")
 public class BoardController {
