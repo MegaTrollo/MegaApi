daf321766a14a23e80485768f116f57e4e8d4b66
Kamil Żarski <209526@edu.p.lodz.pl>
2019-11-14 23:49:56 +0100

    start new implementation authorization

---
+++ src/main/java/com/example/demo/endpoints/AuthorizationController.java
@@ -0,0 +1,15 @@
+package com.example.demo.endpoints;
+
+import com.example.demo.services.UserDetailsServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthorizationController {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+}
